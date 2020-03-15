d030821b3ab53d264ee4b28c9d555f9e653e94eb
imper <imper@windows10.mshome.net>
2020-03-15 15:23:53 +0300

    modified comment

--- app.php
+++ app.php
@@ -4,4 +4,4 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 $app = new \Symfony\Component\Console\Application('demo application');
 $app->add(new \App\UserInput());
 $app->run();
-// This is master branch
+// This is dev-3 branch
