083eeefcadde71b808040f4515afed98a5420b9e
imper <imper@windows10.mshome.net>
2020-03-15 15:35:06 +0300

    modified comment

--- app.php
+++ app.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $app = new \Symfony\Component\Console\Application('demo application');
+$app->add(new \App\PrintString());
 $app->add(new \App\SayHello());
 $app->run();
 // This is dev-1 branch
